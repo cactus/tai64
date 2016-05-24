a92bf8e5b50eadf214b6962d63e88072f1ba739d
elij <elij@wundrd.net>
2016-05-24 02:53:00 -0700

    more compact test strings

--- time_test.go
+++ time_test.go
@@ -12,76 +12,71 @@ import (
 )
 
 var tests = map[string]struct {
-	t time.Time
+	t string
 	o string
 }{
 	"a while ago": {
-		time.Date(1920, 01, 01, 00, 00, 00, 00, time.UTC),
-		"@3fffffffa1f2cd8a00000000",
+		"1920-01-01T00:00:00Z", "@3fffffffa1f2cd8a00000000",
 	},
 	"second before tai swap": {
-		time.Date(1969, 12, 31, 23, 59, 49, 00, time.UTC),
-		"@3fffffffffffffff00000000",
+		"1969-12-31T23:59:49Z", "@3fffffffffffffff00000000",
 	},
 	"second at tai swap": {
-		time.Date(1969, 12, 31, 23, 59, 50, 00, time.UTC),
-		"@400000000000000000000000",
+		"1969-12-31T23:59:50Z", "@400000000000000000000000",
 	},
 	"second after tai swap": {
-		time.Date(1969, 12, 31, 23, 59, 51, 00, time.UTC),
-		"@400000000000000100000000",
+		"1969-12-31T23:59:51Z", "@400000000000000100000000",
 	},
 	"second before unix epoc": {
-		time.Date(1969, 12, 31, 23, 59, 59, 00, time.UTC),
-		"@400000000000000900000000",
+		"1969-12-31T23:59:59Z", "@400000000000000900000000",
 	},
 	"second at unix epoc": {
-		time.Date(1970, 01, 01, 00, 00, 00, 00, time.UTC),
-		"@400000000000000a00000000",
+		"1970-01-01T00:00:00Z", "@400000000000000a00000000",
 	},
 	"second after unix epoc": {
-		time.Date(1970, 01, 01, 00, 00, 01, 00, time.UTC),
-		"@400000000000000b00000000",
+		"1970-01-01T00:00:01Z", "@400000000000000b00000000",
 	},
 	"second before tai-utc epoc": {
-		time.Date(1970, 01, 01, 00, 00, 9, 00, time.UTC),
-		"@400000000000001300000000",
+		"1970-01-01T00:00:09Z", "@400000000000001300000000",
 	},
 	"second at tai-utc epoc": {
-		time.Date(1970, 01, 01, 00, 00, 10, 00, time.UTC),
-		"@400000000000001400000000",
+		"1970-01-01T00:00:10Z", "@400000000000001400000000",
 	},
 	"second after tai-utc epoc": {
-		time.Date(1970, 01, 01, 00, 00, 11, 00, time.UTC),
-		"@400000000000001500000000",
+		"1970-01-01T00:00:11Z", "@400000000000001500000000",
 	},
 	"a more current date": {
-		time.Date(2016, 12, 31, 23, 59, 59, 00, time.UTC),
-		"@40000000586846a300000000",
+		"2016-12-31T23:59:59Z", "@40000000586846a300000000",
 	},
 }
 
 func TestRoundTrip(t *testing.T) {
 	for name, tt := range tests {
-		o := Format(tt.t)
+		tm, err := time.Parse(time.RFC3339Nano, tt.t)
+		assert.Nil(t, err, "%s: test failed parsing time.Time", name)
+		o := Format(tm)
 		p, err := Parse(o)
 		assert.Nil(t, err, "%s: test failed parsing", name)
-		assert.Equal(t, tt.t, p, "%s: test failed date compare: %s", name)
+		assert.Equal(t, tm, p, "%s: test failed date compare: %s", name)
 	}
 }
 
 func TestFormat(t *testing.T) {
 	for name, tt := range tests {
-		o := Format(tt.t)
+		tm, err := time.Parse(time.RFC3339Nano, tt.t)
+		assert.Nil(t, err, "%s: test failed parsing time.Time", name)
+		o := Format(tm)
 		assert.Equal(t, tt.o, o, "%s: test failed date compare", name)
 	}
 }
 
 func TestParse(t *testing.T) {
 	for name, tt := range tests {
+		tm, err := time.Parse(time.RFC3339Nano, tt.t)
+		assert.Nil(t, err, "%s: test failed parsing time.Time", name)
 		p, err := Parse(tt.o)
 		assert.Nil(t, err, "%s: test failed parsing", name)
-		assert.Equal(t, tt.t, p, "%s: test failed date compare", name)
+		assert.Equal(t, tm, p, "%s: test failed date compare", name)
 	}
 }
 
